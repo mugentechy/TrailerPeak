a0e3896d7f3b5b2fd1302bc399c0c7cdcbb2ebb3
mugen <mugentechy@gmail.com>
2025-01-29 20:41:37 +0300

    upate

--- src/components/banner.js
+++ src/components/banner.js
@@ -22,12 +22,14 @@ const Banner = () => {
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + '...' : str
   }
+
+  console.log(movie)
   return (
     <header
       className='banner'
       style={{
         backgroundSize: 'cover',
-        backgroundImage: `url("https:image.tmdb.org/t/p/original${movie?.poster_path}")`,
+        backgroundImage: `url("https:image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPostion: 'center center',
       }}
     >
